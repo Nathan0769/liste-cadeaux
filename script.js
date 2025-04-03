14fdf4fe68b4861312ca0c050898e18048cca7c1
Nathan Rivollier <nathan@MacBook-Pro-de-Nathan.local>
2025-04-03 11:53:00 +0200

    responsive  menu

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Bienvenue sur la liste de cadeaux !");
+const burgerMenuButton = document.querySelector(".menu-burger-button");
+const burgerMenuButtonIcon = document.querySelector(".menu-burger-button i");
+const burgerMenu = document.querySelector(".burger-menu");
+
+burgerMenuButton.onclick = function () {
+  burgerMenu?.classList.toggle("open");
+  const isOpen = burgerMenu?.classList.contains("open");
+
+  if (burgerMenuButtonIcon) {
+    burgerMenuButtonIcon.classList.toggle("fa-bars", !isOpen);
+    burgerMenuButtonIcon.classList.toggle("fa-xmark", isOpen);
+  }
+};
